ea2b21301513477e9e18c8ac08f6ee646eaa4bcf
sabbir <alam.sabbir78692@gmail.com>
2023-05-31 22:42:13 +0530

    initial

---
+++ app/src/main/java/com/example/letsdoit/Splash.java
@@ -0,0 +1,25 @@
+package com.example.letsdoit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent i = new Intent(Splash.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },4000);
+    }
+}
